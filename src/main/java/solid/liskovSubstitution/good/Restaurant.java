13b56724cf8f3667deebb776376b004d38e92495
Kartikeya Sharma <workwithkartikeyasharma@gmail.com>
2025-12-30 22:53:04 +0000

    liskov principle

--- src/main/java/solid/liskovSubstitution/good/Restaurant.java
+++ src/main/java/solid/liskovSubstitution/good/Restaurant.java
@@ -3,15 +3,17 @@ package solid.liskovSubstitution.good;
 
 // Objects of a superclass should be replaceable with objects of its subclasses without breaking the application
 // Story 3 - Cook able Food and Non Cook able food
+// and pizza is type of both food and cookable food
+// salad is just type of Food
 public class Restaurant {
     public static void main(String[] args) {
         System.out.println("Lets order");
         Pizza pizza = new Pizza();
-        pizza.getName();
-        pizza.prepare();
+        pizza.getName(); // yes we can serve pizza
+        pizza.prepare(); // it will actually make pizza
 
         Salad salad = new Salad();
-        salad.getName();
+        salad.getName(); // yes we can serve salad
 
     }
 }
